cd78dd71b2e38f80fd3a0c087d3adcdfee8c7ec6
Daniel Phillips <daniel@tux3.org>
2008-08-06 01:27:45 -0700

    Add inode_lookup test, handle various corner cases for empty leaves
    Signed-off-by: Daniel Phillips <daniel@tux3.org>

--- user/test/ileaf.c
+++ user/test/ileaf.c
@@ -57,11 +57,12 @@ unsigned ileaf_free(struct ileaf *leaf)
 void ileaf_dump(struct ileaf *leaf)
 {
 	u16 *dict = (void *)leaf + blocksize, offset = 0, inum = leaf->inum;
-	printf("%i inodes, free = %i:\n", leaf->count, ileaf_free(leaf));
+	printf("%i inodes, %i free:\n", leaf->count, ileaf_free(leaf));
 	//hexdump(dict - leaf->count, leaf->count * 2);
 	for (int i = -1; i >= -leaf->count; i--, inum++) {
 		int limit = dict[i], size = limit - offset;
-		printf("  %i: [%i] ", inum, offset);
+		printf("  %i: ", inum);
+		//printf("[%i] ", offset);
 		if (size < 0)
 			printf("<corrupt>\n");
 		else if (size == 0)
@@ -72,16 +73,14 @@ void ileaf_dump(struct ileaf *leaf)
 	}
 }
 
-unsigned ileaf_lookup(struct ileaf *leaf, inum_t inum, unsigned *size)
+void *ileaf_lookup(struct ileaf *leaf, inum_t inum, unsigned *size)
 {
 	assert(inum > leaf->inum);
 	unsigned at = inum - leaf->inum;
-	if (at < leaf->count) {
-		*size = 0;
-		return 0;
-	}
+	printf("inode at %i\n", at);
+	assert(at < leaf->count);
 	u16 *dict = (void *)leaf + blocksize, offset = (at ? *(dict - at) : 0);
-	return (*size = *(dict - at - 1) - offset) ? offset : 0;
+	return (*size = *(dict - at - 1) - offset) ? leaf->table + offset : NULL;
 }
 
 int ileaf_check(struct ileaf *leaf)
@@ -96,11 +95,19 @@ eek:
 	return -1;
 }
 
+void ileaf_trim(struct ileaf *leaf) {
+	u16 *dict = (void *)leaf + blocksize;
+	while (leaf->count > 1 && *(dict - leaf->count) == *(dict - leaf->count + 1))
+		leaf->count--;
+	if (leaf->count == 1 && !*(dict - 1))
+		leaf->count = 0;
+}
+
 void ileaf_split(struct ileaf *leaf, struct ileaf *dest, int fudge)
 {
 	u16 *dict = (void *)leaf + blocksize, *destdict = (void *)dest + blocksize;
 
-	/* find inum nearest block middle */
+	/* binsearch inum nearest middle */
 	unsigned at = 1, hi = leaf->count;
 	while (at < hi) {
 		int mid = (at + hi) / 2;
@@ -109,39 +116,44 @@ void ileaf_split(struct ileaf *leaf, struct ileaf *dest, int fudge)
 		else
 			hi = mid;
 	}
+	printf("split at %i\n", (blocksize / 2) + fudge);
+
+	/* should trim leading empty inodes on copy */
 	unsigned split = *(dict - at), free = *(dict - leaf->count);
-	printf("split out %i..%i\n", split, free);
+	printf("copy out %i bytes at %i\n", free - split, split);
 	assert(free >= split);
 	memcpy(dest->table, leaf->table + split, free - split);
 	dest->count = leaf->count - at;
 	veccopy(destdict - dest->count, dict - leaf->count, dest->count);
-	leaf->count = at;
-	memset(leaf->table + split, 0, (char *)(dict - leaf->count) - (leaf->table + split));
 	for (int i = 1; i <= dest->count; i++)
 		*(destdict - i) -= *(dict - at);
 	dest->inum = leaf->inum + at;
+	leaf->count = at;
+	memset(leaf->table + split, 0, (char *)(dict - leaf->count) - (leaf->table + split));
+	ileaf_trim(leaf);
 }
 
 void ileaf_merge(struct ileaf *leaf, struct ileaf *from)
 {
+	if (!from->count)
+		return;
 	u16 *dict = (void *)leaf + blocksize, *fromdict = (void *)from + blocksize;
-	unsigned at = leaf->count, free = *(dict - at), size = *(fromdict - from->count);
-	printf("copy in %i bytes\n", size);
+	unsigned at = leaf->count, free = at ? *(dict - at) : 0;
+	unsigned size = from->count ? *(fromdict - from->count) : 0;
+	printf("copy in %i bytes %i %i\n", size, at + 1, at + from->count);
 	memcpy(leaf->table + free, from->table, size);
 	veccopy(dict - (leaf->count += from->count), fromdict - from->count, from->count);
-	for (int i = at + 1; i <= at + from->count; i++)
+	for (int i = at + 1; at && i <= at + from->count; i++)
 		*(dict - i) += *(dict - at);
 }
 
 void *inode_expand(struct ileaf *leaf, inum_t inum, unsigned more, char fill)
 {
-	//unsigned size = 0, offset = ileaf_lookup(leaf, inum, &size);
-
 	assert(inum > leaf->inum);
 	u16 *dict = (void *)leaf + blocksize;
 	unsigned at = inum - leaf->inum;
 
-	/* extend dict as necessary */
+	/* extend with empty inodes */
 	while (leaf->count <= at) {
 		*(dict - leaf->count - 1) = leaf->count ? *(dict - leaf->count) : 0;
 		leaf->count++;
@@ -168,7 +180,7 @@ int main(int argc, char *argv[])
 	inode_expand(leaf, 4, 4, 'b');
 	inode_expand(leaf, 6, 6, 'c');
 	ileaf_dump(leaf);
-	ileaf_split(leaf, dest, 0);
+	ileaf_split(leaf, dest, -(blocksize / 2));
 	ileaf_dump(leaf);
 	ileaf_dump(dest);
 	ileaf_merge(leaf, dest);
@@ -177,5 +189,10 @@ int main(int argc, char *argv[])
 	ileaf_dump(leaf);
 	inode_expand(leaf, 8, 3, 'y');
 	ileaf_dump(leaf);
+	unsigned size = 0;
+	char *inode = ileaf_lookup(leaf, 3, &size);
+	hexdump(inode, size);
+	ileaf_destroy(leaf);
+	ileaf_destroy(dest);
 	return 0;
 }
