a40577aac2b3e7f8615e1e0f2cd4ef098caf5a54
Daniel Phillips <daniel@tux3.org>
2008-09-07 01:33:37 -0700

    Convert some printfs to traces.  Make error an expression so you can write return error(...) from main.  Probably not a good idea, on probation.
    Signed-off-by: Daniel Phillips <daniel@tux3.org>

--- user/test/trace.h
+++ user/test/trace.h
@@ -20,7 +20,7 @@ static inline void logline(const char *caller, const char *fmt, ...)
 
 //#define die(code) exit(code)
 #define die(code) asm("int3")
-#define error(string, args...) do { warn(string, ##args); die(99); } while (0) 
+#define error(string, args...) ({ warn(string, ##args); die(99); 1; })
 #define assert(expr) do { if (!(expr)) error("Failed assertion \"%s\"\n", #expr); } while (0)
 #define warn(string, args...) do { logline(__func__, string, ##args); } while (0)
 #define trace_off(...)
