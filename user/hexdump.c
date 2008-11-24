d643494ce056278ef0f850abc9f495adc70dc939
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2008-11-24 18:27:15 +0900

    Share user/kernel/hexdump.c functions with user/hexdump.c
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
    Signed-off-by: Daniel Phillips <daniel@tux3.org>

--- user/hexdump.c
+++ user/hexdump.c
@@ -5,25 +5,6 @@
 
 #include "tux3.h"
 
-void hexdump(void *data, unsigned size)
-{
-	while (size) {
-		unsigned char *p;
-		int w = 16, n = size < w? size: w, pad = w - n;
-		printf("%p:  ", data);
-		for (p = data; p < (unsigned char *)data + n;)
-			printf("%02hx ", *p++);
-		printf("%*.s  \"", pad*3, "");
-		for (p = data; p < (unsigned char *)data + n;) {
-			int c = *p++;
-			printf("%c", c < ' ' || c > 127 ? '.' : c);
-		}
-		printf("\"\n");
-		data += w;
-		size -= n;
-	}
-}
-
 #ifndef __KERNEL__
 #include <execinfo.h>
 
@@ -35,4 +16,6 @@ void stacktrace(void)
 	backtrace_symbols_fd(array, size, 2);
 }
 #endif
+
+#include "kernel/hexdump.c"
 #endif
--- user/kernel/hexdump.c
+++ user/kernel/hexdump.c
@@ -1,8 +1,5 @@
 /* Copyright (c) 2008 Daniel Phillips <phillips@phunq.net>, GPL v3 */
 
-#ifndef HEXDUMP
-#define HEXDUMP
-
 #include "tux3.h"
 
 void hexdump(void *data, unsigned size)
@@ -23,16 +20,3 @@ void hexdump(void *data, unsigned size)
 		size -= n;
 	}
 }
-
-#ifndef __KERNEL__
-#include <execinfo.h>
-
-void stacktrace(void)
-{
-	void *array[100];
-	size_t size = backtrace(array, 100);
-	printf("_______stack ______\n");
-	backtrace_symbols_fd(array, size, 2);
-}
-#endif
-#endif
