b93e2c8640c3fb655b2fcb12e5212a32a00b29c0
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2008-11-24 18:27:15 +0900

    Share user/kernel/balloc.c functions with user/balloc.c
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
    Signed-off-by: Daniel Phillips <daniel@tux3.org>

--- user/kernel/balloc.c
+++ user/kernel/balloc.c
@@ -9,19 +9,6 @@
  * the right to distribute those changes under any license.
  */
 
-#ifndef __KERNEL__
-#include <stdio.h>
-#include <inttypes.h>
-#include <string.h>
-#include <stdlib.h>
-#include <errno.h>
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include "hexdump.c"
-#endif
-
 #include "tux3.h"
 
 #ifndef trace
@@ -294,81 +281,3 @@ void bfree(SB, block_t block)
 {
 	bfree_extent(sb, block, 1);
 }
-
-#ifndef __KERNEL__
-#ifndef main
-int main(int argc, char *argv[])
-{
-	if (1) {
-		warn("---- test bitops ----");
-		unsigned char bits[16];
-		memset(bits, 0, sizeof(bits));
-		set_bits(bits, 6, 20);
-		set_bits(bits, 49, 16);
-		set_bits(bits, 0x51, 2);
-		hexdump(bits, sizeof(bits));
-		/* should return true */
-		printf("ones = %i\n", all_set(bits, 6, 20));
-		printf("ones = %i\n", all_set(bits, 49, 16));
-		printf("ones = %i\n", all_set(bits, 0x51, 2));
-		/* should return false */
-		printf("ones = %i\n", all_set(bits, 5, 20));
-		printf("ones = %i\n", all_set(bits, 49, 17));
-		printf("ones = %i\n", all_set(bits, 0x51, 3));
-		clear_bits(bits, 6, 20);
-		clear_bits(bits, 49, 16);
-		clear_bits(bits, 0x51, 2);
-		hexdump(bits, sizeof(bits)); // all zero now
-
-		/* corner case */
-		unsigned char *bitmap = malloc(7);
-		set_bits(bitmap, 0, 7 * 8);
-		int ret = all_set(bitmap, 0, 7 * 8);
-		assert(ret);
-		clear_bits(bitmap, 0, 7 * 8);
-		free(bitmap);
-	}
-	struct dev *dev = &(struct dev){ .bits = 3 };
-	map_t *map = new_map(dev, NULL);
-	struct sb *sb = &(struct sb){ .super = { .volblocks = to_be_u64(150) }, .blockbits = dev->bits };
-	struct inode *bitmap = &(struct inode){ .i_sb = sb, .map = map };
-	sb->freeblocks = from_be_u64(sb->super.volblocks);
-	sb->nextalloc = from_be_u64(sb->super.volblocks); // this should wrap around to zero
-	sb->bitmap = bitmap;
-
-	init_buffers(dev, 1 << 20);
-	unsigned blocksize = 1 << dev->bits;
-	unsigned dumpsize = blocksize > 16 ? 16 : blocksize;
-
-	for (int block = 0; block < 10; block++) {
-		struct buffer_head *buffer = blockget(map, block);
-		memset(bufdata(buffer), 0, blocksize);
-		set_buffer_uptodate(buffer);
-	}
-	for (int i = 0; i < 12; i++) {
-		block_t block = balloc_from_range(bitmap, 121, 10);
-		printf("%Li\n", (L)block);
-	}
-	hexdump(blockget(map, 0)->data, dumpsize);
-	hexdump(blockget(map, 1)->data, dumpsize);
-	hexdump(blockget(map, 2)->data, dumpsize);
-
-	sb->nextalloc++; // gap
-	for (int i = 0; i < 1; i++)
-		balloc(sb);
-	sb->nextalloc++; // gap
-	for (int i = 0; i < 10; i++)
-		balloc(sb);
-	hexdump(blockget(map, 0)->data, dumpsize);
-	hexdump(blockget(map, 1)->data, dumpsize);
-	hexdump(blockget(map, 2)->data, dumpsize);
-
-	bitmap_dump(bitmap, 0, from_be_u64(sb->super.volblocks));
-	printf("%Li used, %Li free\n", (L)count_range(bitmap, 0, from_be_u64(sb->super.volblocks)), (L)sb->freeblocks);
-	bfree(sb, 0x7e);
-	bfree(sb, 0x80);
-	bitmap_dump(bitmap, 0, from_be_u64(sb->super.volblocks));
-	return 0;
-}
-#endif
-#endif /* !__KERNEL__ */
