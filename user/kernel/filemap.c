253ed856c2f4e3e2b46c7e35b877b300aeb66098
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2008-11-25 10:45:41 +0900

    Add writepage handlers
    With this, we can overwrite data blocks.
    
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
    Signed-off-by: Daniel Phillips <daniel@tux3.org>

--- user/kernel/filemap.c
+++ user/kernel/filemap.c
@@ -1,6 +1,7 @@
 #ifdef __KERNEL__
 #include <linux/fs.h>
 #include <linux/mpage.h>
+#include <linux/aio.h>
 #endif
 #include "tux3.h"
 
@@ -238,7 +239,12 @@ eek:
 int tux3_get_block(struct inode *inode, sector_t iblock,
 		   struct buffer_head *bh_result, int create)
 {
-	if (create)
+	int blocksize = 1 << inode->i_blkbits;
+	sector_t last_iblock;
+
+	last_iblock = (i_size_read(inode) + (blocksize - 1))
+		>> inode->i_blkbits;
+	if (iblock >= last_iblock)
 		return -EIO;
 
 	struct sb *sbi = tux_sb(inode->i_sb);
@@ -410,6 +416,36 @@ static int tux3_readpages(struct file *file, struct address_space *mapping,
 	return mpage_readpages(mapping, pages, nr_pages, tux3_get_block);
 }
 
+static int tux3_write_begin(struct file *file, struct address_space *mapping,
+			    loff_t pos, unsigned len, unsigned flags,
+			    struct page **pagep, void **fsdata)
+{
+	*pagep = NULL;
+	return block_write_begin(file, mapping, pos, len, flags, pagep, fsdata,
+				 tux3_get_block);
+}
+
+static int tux3_writepage(struct page *page, struct writeback_control *wbc)
+{
+	return block_write_full_page(page, tux3_get_block, wbc);
+}
+
+static int tux3_writepages(struct address_space *mapping,
+			   struct writeback_control *wbc)
+{
+	return mpage_writepages(mapping, wbc, tux3_get_block);
+}
+
+static ssize_t tux3_direct_IO(int rw, struct kiocb *iocb,
+			      const struct iovec *iov,
+			      loff_t offset, unsigned long nr_segs)
+{
+	struct file *file = iocb->ki_filp;
+	struct inode *inode = file->f_mapping->host;
+	return blockdev_direct_IO(rw, iocb, inode, inode->i_sb->s_bdev, iov,
+				  offset, nr_segs, tux3_get_block, NULL);
+}
+
 static sector_t tux3_bmap(struct address_space *mapping, sector_t iblock)
 {
 	sector_t blocknr;
@@ -424,16 +460,16 @@ static sector_t tux3_bmap(struct address_space *mapping, sector_t iblock)
 const struct address_space_operations tux_aops = {
 	.readpage		= tux3_readpage,
 	.readpages		= tux3_readpages,
-//	.writepage		= ext4_da_writepage,
-//	.writepages		= ext4_da_writepages,
-//	.sync_page		= block_sync_page,
-//	.write_begin		= ext4_da_write_begin,
-//	.write_end		= ext4_da_write_end,
+	.writepage		= tux3_writepage,
+	.writepages		= tux3_writepages,
+	.sync_page		= block_sync_page,
+	.write_begin		= tux3_write_begin,
+	.write_end		= generic_write_end,
 	.bmap			= tux3_bmap,
 //	.invalidatepage		= ext4_da_invalidatepage,
 //	.releasepage		= ext4_releasepage,
-//	.direct_IO		= ext4_direct_IO,
-//	.migratepage		= buffer_migrate_page,
+	.direct_IO		= tux3_direct_IO,
+	.migratepage		= buffer_migrate_page,
 //	.is_partially_uptodate	= block_is_partially_uptodate,
 };
 
