6be819a873eb244daafd382dc106816ae823446d
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2012-08-18 23:57:32 +0900

    Add balloc_from_range() to balloc-dummy.c
    We have to provide balloc_from_range() to replace balloc.c completely.
    
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>

--- user/tests/balloc-dummy.c
+++ user/tests/balloc-dummy.c
@@ -1,8 +1,21 @@
+block_t balloc_from_range(struct sb *sb, block_t start, unsigned count, unsigned blocks)
+{
+	block_t block = sb->nextalloc;
+	sb->nextalloc += blocks;
+	trace("-> %Lx/%x", (L)block, blocks);
+	return block;
+}
+
 int balloc(struct sb *sb, unsigned blocks, block_t *block)
 {
-	*block = sb->nextalloc;
-	sb->nextalloc += blocks;
-	trace("-> %Lx/%x", (L)*block, blocks);
+	block_t goal = sb->nextalloc, total = sb->volblocks;
+
+	if ((*block = balloc_from_range(sb, goal, total - goal, blocks)) >= 0)
+		goto found;
+	if ((*block = balloc_from_range(sb, 0, goal, blocks)) >= 0)
+		goto found;
+	return -ENOSPC;
+found:
 	return 0;
 }
 
