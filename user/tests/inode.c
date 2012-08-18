2ccf9e51b825fce30da2542677781df50509a15b
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2012-08-18 23:57:53 +0900

    Rewrite tests/inode.c with test framework
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>

--- user/tests/inode.c
+++ user/tests/inode.c
@@ -1,22 +1,143 @@
 #include "tux3user.h"
 #include "diskio.h"	/* for fdsize64() */
+#include "test.h"
+
+#ifndef trace
+#define trace trace_off
+#endif
 
 #include "../inode.c"
+
+static void clean_main(struct sb *sb)
+{
+	put_super(sb);
+}
+
+static void test01(struct sb *sb)
+{
+	struct inode *inode;
+	struct tux_iattr iattr = { .mode = S_IFREG | S_IRWXU };
+
+	char name[] = "foo";
+
+	/* Test create */
+	inode = tuxcreate(sb->rootdir, name, strlen(name), &iattr);
+	test_assert(!IS_ERR(inode));
+
+	struct file *file = &(struct file){ .f_inode = inode };
+	int seek_pos = 4092;
+	char buf[] = "hello world!";
+	int size = strlen(buf);
+	int low_size = size / 2;
+	int high_size = size - low_size;
+	int err, got;
+
+	/* Test write data */
+	tuxseek(file, seek_pos);
+	got = tuxwrite(file, buf, low_size);
+	test_assert(got == low_size);
+	got = tuxwrite(file, buf + low_size, high_size);
+	test_assert(got == high_size);
+	/* Test write xattr */
+	err = set_xattr(inode, name, strlen(name), buf, size, 0);
+	test_assert(!err);
+
+	iput(inode);
+	sync_inode(inode);
+
+	/* Check create */
+	inode = tuxopen(sb->rootdir, name, strlen(name));
+	test_assert(!IS_ERR(inode));
+	/* Check data */
+	file = &(struct file){ .f_inode = inode, };
+	tuxseek(file, seek_pos);
+	char data[100];
+	memset(data, 0, sizeof(data));
+	got = tuxread(file, data, sizeof(data));
+	test_assert(got == size);
+	test_assert(!memcmp(data, buf, size));
+	/* Check xattr */
+	got = get_xattr(inode, name, strlen(name), data, sizeof(data));
+	test_assert(got == size);
+	test_assert(!memcmp(data, buf, size));
+	iput(inode);
+
+	force_delta(sb);
+	clean_main(sb);
+}
+
+/* Test try to allocate same inum */
+static void test02(struct sb *sb)
+{
+	struct tux_iattr *iattr = &(struct tux_iattr){};
+	struct inode *inode1, *inode2, *inode3, *inode4;
+	int err;
+
+	/* Both is deferred allocation */
+	inode1 = __tux_create_inode(sb->rootdir, 0x1000, iattr, 0);
+	test_assert(inode1);
+	test_assert(is_defer_alloc_inum(inode1));
+	inode2 = __tux_create_inode(sb->rootdir, 0x1000, iattr, 0);
+	test_assert(inode2);
+	test_assert(is_defer_alloc_inum(inode2));
+
+	/* Test inum allocation */
+	test_assert(inode1->inum != inode2->inum);
+	/* Save first inode */
+	err = sync_inode(inode1);
+	test_assert(!err);
+	test_assert(!is_defer_alloc_inum(inode1));
+
+	/* Try to alloc same inum after save */
+	inode3 = __tux_create_inode(sb->rootdir, 0x1000, iattr, 0);
+	test_assert(inode3);
+	test_assert(is_defer_alloc_inum(inode3));
+	/* Try to alloc so far inum */
+	inode4 = __tux_create_inode(sb->rootdir, 0x10000000, iattr, 0);
+	test_assert(inode4);
+	test_assert(is_defer_alloc_inum(inode4));
+	/* Save inodes */
+	err = sync_inode(inode2);
+	test_assert(!err);
+	test_assert(!is_defer_alloc_inum(inode2));
+	err = sync_inode(inode3);
+	test_assert(!err);
+	test_assert(!is_defer_alloc_inum(inode3));
+
+	/* Test inum allocation */
+	test_assert(inode1->inum == 0x1000);
+	test_assert(inode2->inum == 0x1001);
+	test_assert(inode3->inum == 0x1002);
+	test_assert(inode4->inum == 0x10000000);
+	iput(inode1);
+	iput(inode2);
+	iput(inode3);
+
+	/* Delete deferred allocation inode */
+	inode4->i_nlink--;
+	test_assert(is_defer_alloc_inum(inode4));
+	err = tux_delete_inode(inode4);
+	test_assert(!err);
+
+	force_rollup(sb);
+
+	clean_main(sb);
+}
 
 int main(int argc, char *argv[])
 {
 	if (argc < 2)
 		error("usage: %s <volname>", argv[0]);
-	int err = 0;
+
 	char *name = argv[1];
 	int fd = open(name, O_CREAT|O_TRUNC|O_RDWR, S_IRUSR|S_IWUSR);
 	assert(!ftruncate(fd, 1 << 24));
 	u64 size = 0;
-	if (fdsize64(fd, &size))
-		error("fdsize64 failed for '%s' (%s)", name, strerror(errno));
+	int err = fdsize64(fd, &size);
+	assert(!err);
 
 	struct dev *dev = &(struct dev){ .fd = fd, .bits = 12 };
-	init_buffers(dev, 1 << 20, 0);
+	init_buffers(dev, 1 << 24, 2);
 
 	struct disksuper super = INIT_DISKSB(dev->bits, size >> dev->bits);
 	struct sb *sb = rapid_sb(dev);
@@ -28,93 +149,19 @@ int main(int argc, char *argv[])
 	sb->logmap = tux_new_logmap(sb);
 	assert(sb->logmap);
 
-	trace("make tux3 filesystem on %s (0x%Lx bytes)", name, (L)size);
-	if ((errno = -make_tux3(sb)))
-		goto eek;
-	trace("create file");
-	struct inode *inode = tuxcreate(sb->rootdir, "foo", 3, &(struct tux_iattr){ .mode = S_IFREG | S_IRWXU });
-	if (IS_ERR(inode))
-		exit(1);
-	tux_dump_entries(blockget(mapping(sb->rootdir), 0));
+	err = make_tux3(sb);
+	assert(!err);
 
-	trace(">>> write file");
-	char buf[100] = { };
-	struct file *file = &(struct file){ .f_inode = inode };
-	tuxseek(file, (1LL << 60) - 12);
-	tuxseek(file, 4092);
-	err = tuxwrite(file, "hello ", 6);
-	err = tuxwrite(file, "world!", 6);
-#if 0
-	flush_buffers(mapping(sb->bitmap));
-	flush_buffers(sb->volmap->map);
-#endif
-#if 1
-	trace(">>> close file <<<");
-	set_xattr(inode, "foo", 5, "hello world!", 12, 0);
-	sync_inode(inode);
-	iput(inode);
-	trace(">>> open file");
-	file = &(struct file){ .f_inode = tuxopen(sb->rootdir, "foo", 3) };
-	inode = file->f_inode;
-	xcache_dump(inode);
-#endif
-	trace(">>> read file");
-	tuxseek(file, (1LL << 60) - 12);
-	tuxseek(file, 4092);
-	memset(buf, 0, sizeof(buf));
-	int got = tuxread(file, buf, sizeof(buf));
-	trace_off("got %x bytes", got);
-	if (got < 0)
-		exit(1);
-	hexdump(buf, got);
-	trace(">>> show state");
-	show_buffers(mapping(file->f_inode));
-	show_buffers(mapping(sb->rootdir));
-	show_buffers(sb->volmap->map);
-	bitmap_dump(sb->bitmap, 0, sb->volblocks);
-	show_tree_range(itable_btree(sb), 0, -1);
+	test_init(argv[0]);
 
-	if (1) { /* try to allocate same inum */
-		struct tux_iattr *iattr = &(struct tux_iattr){};
-		struct inode *inode1, *inode2, *inode3, *inode4;
-		/* both is deferred allocation */
-		inode1 = __tux_create_inode(sb->rootdir, 0x1000, iattr, 0);
-		assert(inode1);
-		inode2 = __tux_create_inode(sb->rootdir, 0x1000, iattr, 0);
-		assert(inode2);
-		/* test inum allocation */
-		assert(inode1->inum != inode2->inum);
-		/* save first inode */
-		err = sync_inode(inode1);
-		assert(!err);
-		/* try to alloc same inum after save */
-		inode3 = __tux_create_inode(sb->rootdir, 0x1000, iattr, 0);
-		assert(inode3);
-		/* try to alloc so far inum */
-		inode4 = __tux_create_inode(sb->rootdir, 0x10000000, iattr, 0);
-		assert(inode4);
-		/* save inodes */
-		err = sync_inode(inode2);
-		assert(!err);
-		err = sync_inode(inode3);
-		assert(!err);
-		/* test inum allocation */
-		assert(inode1->inum == 0x1000);
-		assert(inode2->inum == 0x1001);
-		assert(inode3->inum == 0x1002);
-		assert(inode4->inum == 0x10000000);
-		iput(inode1);
-		iput(inode2);
-		iput(inode3);
-		/* delete deferred allocation inode */
-		inode4->i_nlink--;
-		tux_delete_inode(inode4);
-	}
+	if (test_start("test01"))
+		test01(sb);
+	test_end();
 
-	destroy_defer_bfree(&sb->derollup);
-	destroy_defer_bfree(&sb->defree);
+	if (test_start("test02"))
+		test02(sb);
+	test_end();
 
-	exit(0);
-eek:
-	return error("Eek! %s", strerror(errno));
+	clean_main(sb);
+	return test_failures();
 }
