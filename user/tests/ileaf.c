d57501badfa0a8420174c52b13219e778aa3e49a
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2012-08-18 23:57:52 +0900

    Rewrite tests/ileaf.c with test framework
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>

--- user/tests/ileaf.c
+++ user/tests/ileaf.c
@@ -9,6 +9,7 @@
  */
 
 #include "tux3user.h"
+#include "test.h"
 
 #ifndef trace
 #define trace trace_off
@@ -19,7 +20,8 @@
 static struct ileaf *ileaf_create(struct btree *btree)
 {
 	struct ileaf *leaf = malloc(btree->sb->blocksize);
-	ileaf_init(btree, leaf);
+	assert(leaf);
+	btree->ops->leaf_init(btree, leaf);
 	return leaf;
 }
 
@@ -33,7 +35,6 @@ static void test_append(struct btree *btree, struct ileaf *leaf, inum_t inum, in
 {
 	unsigned size = 0;
 	char *attrs = ileaf_lookup(btree, inum, leaf, &size);
-	printf("attrs %p, attrs size = %i\n", attrs, size);
 	attrs = ileaf_resize(btree, inum, leaf, size + more);
 	memset(attrs + size, fill, more);
 }
@@ -46,6 +47,124 @@ static void test_remove(struct btree *btree, struct ileaf *leaf, inum_t inum, in
 	attrs = ileaf_resize(btree, inum, leaf, size - less);
 }
 
+#define check_ileaf_with_data(btree, leaf, d) do {			\
+	for (int i = 0; i < ARRAY_SIZE(d); i++) {			\
+		void *__a;						\
+		unsigned __s;						\
+		__a = ileaf_lookup(btree, (d)[i].inum, leaf, &__s); \
+		if ((d)[i].size == 0)					\
+			test_assert(__a == NULL);			\
+		else {							\
+			test_assert(__a);				\
+			test_assert(__s == (d)[i].size);		\
+			test_assert(!memcmp((d)[i].buf, __a, (d)[i].size)); \
+		}							\
+	}								\
+} while (0)
+
+
+/* Test basic ileaf operations */
+static void test01(struct sb *sb, struct btree *btree)
+{
+
+	struct ileaf *leaf = ileaf_create(btree);
+	struct ileaf *dest = ileaf_create(btree);
+	void *attrs;
+	unsigned size, more;
+
+	struct {
+		inum_t inum;
+		int size;
+		unsigned char c;
+		unsigned char buf[32];
+	} data[] = {
+		{ .inum = 0x10, .size = 0, .c = 'n', },
+		{ .inum = 0x13, .size = 2, .c = 'a', },
+		{ .inum = 0x14, .size = 4, .c = 'b', },
+		{ .inum = 0x16, .size = 6, .c = 'c', },
+		{ .inum = 0x18, .size = 0, .c = 'y', },
+	};
+
+	/* Init data[] */
+	for (int i = 0; i < ARRAY_SIZE(data); i++)
+		memset(data[i].buf, data[i].c, data[i].size);
+
+	leaf->ibase = to_be_u64(0x10);
+	/* Add data */
+	for (int i = 0; i < ARRAY_SIZE(data); i++) {
+		if (data[i].size == 0)
+			continue;
+		test_append(btree, leaf, data[i].inum, data[i].size, data[i].c);
+	}
+	/* Check */
+	check_ileaf_with_data(btree, leaf, data);
+
+	/* Split leaf */
+	inum_t dest_base = ileaf_split(btree, 0x10, leaf, dest);
+	test_assert(dest_base == 0x10);
+	/* Check leaf and dest */
+	for (int i = 0; i < ARRAY_SIZE(data); i++) {
+		attrs = ileaf_lookup(btree, data[i].inum, leaf, &size);
+		test_assert(attrs == NULL);
+		test_assert(size == 0);
+	}
+	check_ileaf_with_data(btree, dest, data);
+
+	/* Merge leaf and dest */
+	ileaf_merge(btree, leaf, dest);
+	/* Check leaf */
+	check_ileaf_with_data(btree, leaf, data);
+
+	/* Change attribute */
+	more = 2;
+	memset(data[0].buf + data[0].size, 'x', more);
+	data[0].size += more;
+	test_append(btree, leaf, data[0].inum, more, 'x');
+	/* Check */
+	check_ileaf_with_data(btree, leaf, data);
+
+	/* Add new inode */
+	more = 3;
+	memset(data[4].buf, data[4].c, more);
+	data[4].size += more;
+	test_append(btree, leaf, data[4].inum, more, data[4].c);
+	/* Check */
+	check_ileaf_with_data(btree, leaf, data);
+
+	/* Shrink attribute */
+	more = 5;
+	data[3].size -= more;
+	test_remove(btree, leaf, data[3].inum, more);
+	/* Check */
+	check_ileaf_with_data(btree, leaf, data);
+
+	/* Test find_empty_inode() */
+	for (int i = 0x11; i <= 0x20; i++) {
+		inum_t alloc = find_empty_inode(btree, leaf, i);
+
+		inum_t expected = i;
+		for (int j = 0; j < ARRAY_SIZE(data); j++) {
+			if (expected == data[j].inum)
+				expected++;
+		}
+
+		test_assert(alloc == expected);
+	}
+
+	/* Remove inode */
+	data[2].size = 0;
+	ileaf_purge(btree, data[2].inum, leaf);
+	data[4].size = 0;
+	ileaf_purge(btree, data[4].inum, leaf);
+	/* Check */
+	check_ileaf_with_data(btree, leaf, data);
+
+	test_assert(ileaf_check(btree, leaf) == 0);
+
+	ileaf_destroy(btree, leaf);
+	ileaf_destroy(btree, dest);
+}
+
 int main(int argc, char *argv[])
 {
 	struct dev *dev = &(struct dev){ .bits = 12 };
@@ -54,41 +173,14 @@ int main(int argc, char *argv[])
 	sb->super = super;
 	setup_sb(sb, &super);
 
-	printf("--- test inode table leaf methods ---\n");
-	struct btree *btree = &(struct btree){
-		.sb = sb,
-		.ops = &itable_ops,
-		.entries_per_leaf = 64, // !!! should depend on blocksize
-	};
-	struct ileaf *leaf = ileaf_create(btree);
-	struct ileaf *dest = ileaf_create(btree);
-	leaf->ibase = to_be_u64(0x10);
-	ileaf_dump(btree, leaf);
-	test_append(btree, leaf, 0x13, 2, 'a');
-	test_append(btree, leaf, 0x14, 4, 'b');
-	test_append(btree, leaf, 0x16, 6, 'c');
-	ileaf_dump(btree, leaf);
-	ileaf_split(btree, 0x10, leaf, dest);
-	ileaf_dump(btree, leaf);
-	ileaf_dump(btree, dest);
-	ileaf_merge(btree, leaf, dest);
-	ileaf_dump(btree, leaf);
-	test_append(btree, leaf, 0x13, 3, 'x');
-	ileaf_dump(btree, leaf);
-	test_append(btree, leaf, 0x18, 3, 'y');
-	ileaf_dump(btree, leaf);
-	test_remove(btree, leaf, 0x16, 5);
-	ileaf_dump(btree, leaf);
-	unsigned size = 0;
-	char *inode = ileaf_lookup(btree, 0x13, leaf, &size);
-	hexdump(inode, size);
-	for (int i = 0x11; i <= 0x20; i++)
-		printf("goal 0x%x => 0x%Lx\n", i, (L)find_empty_inode(btree, leaf, i));
-	ileaf_purge(btree, 0x14, leaf);
-	ileaf_purge(btree, 0x18, leaf);
-	ileaf_check(btree, leaf);
-	ileaf_dump(btree, leaf);
-	ileaf_destroy(btree, leaf);
-	ileaf_destroy(btree, dest);
-	exit(0);
+	struct btree btree;
+	init_btree(&btree, sb, no_root, &itable_ops);
+
+	test_init(argv[0]);
+
+	if (test_start("test01"))
+		test01(sb, &btree);
+	test_end();
+
+	return test_failures();
 }
