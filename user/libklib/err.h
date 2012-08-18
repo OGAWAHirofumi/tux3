33a50fd174f32249fefbf17b37451b974233b20a
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2012-08-18 23:58:39 +0900

    libklib, Update err.h
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>

--- user/libklib/err.h
+++ user/libklib/err.h
@@ -1,29 +1,44 @@
 #ifndef LIBKLIB_ERR_H
 #define LIBKLIB_ERR_H
 
+#include <libklib/compiler.h>
+
 /* Ripped from Linux Kernel by D.Phillips, GPL v2 */
 
 #define MAX_ERRNO       4095
 #define IS_ERR_VALUE(x) ((x) >= (unsigned long)-MAX_ERRNO)
 
-static inline void *ERR_PTR(long error)
+static inline void * __must_check ERR_PTR(long error)
 {
 	return (void *)error;
 }
 
-static inline long PTR_ERR(const void *ptr)
+static inline long __must_check PTR_ERR(const void *ptr)
 {
 	return (long)ptr;
 }
 
-static inline long IS_ERR(const void *ptr)
+static inline long __must_check IS_ERR(const void *ptr)
 {
 	return IS_ERR_VALUE((unsigned long)ptr);
 }
 
-static inline void *ERR_CAST(const void *ptr)
+static inline long __must_check IS_ERR_OR_NULL(const void *ptr)
+{
+	return !ptr || IS_ERR_VALUE((unsigned long)ptr);
+}
+
+static inline void * __must_check ERR_CAST(const void *ptr)
 {
 	return (void *)ptr;
+}
+
+static inline int __must_check PTR_RET(const void *ptr)
+{
+	if (IS_ERR(ptr))
+		return PTR_ERR(ptr);
+	else
+		return 0;
 }
 
 #endif /* !LIBKLIB_ERR_H */
