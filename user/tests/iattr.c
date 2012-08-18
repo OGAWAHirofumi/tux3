5594ef0f794a42cf33511c862067323134a7cb4c
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2012-08-18 23:57:52 +0900

    Rewrite tests/iattr.c with test framework
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>

--- user/tests/iattr.c
+++ user/tests/iattr.c
@@ -9,6 +9,7 @@
  */
 
 #include "tux3user.h"
+#include "test.h"
 
 #ifndef trace
 #define trace trace_off
@@ -16,9 +17,58 @@
 
 #include "kernel/iattr.c"
 
+/* Test encode_attrs() and decode_attrs() */
+static void test01(struct sb *sb)
+{
+	unsigned abits = RDEV_BIT|MODE_OWNER_BIT|DATA_BTREE_BIT|CTIME_SIZE_BIT|LINK_COUNT_BIT|MTIME_BIT;
+	struct inode *inode1 = rapid_open_inode(sb, NULL, S_IFCHR | 0644,
+		.present	= abits,
+		.i_rdev		= MKDEV(1, 3),
+		.i_uid		= 0x12121212,
+		.i_gid		= 0x34343434,
+		.i_size		= 0x123456789ULL,
+		/* FIXME: fixed32 can't represent nanoseconds fully */
+//		.i_ctime	= spectime(0xdec0de01dec0de02ULL),
+		.i_ctime	= spectime(0xdec0de01dec00000ULL),
+//		.i_mtime	= spectime(0xbadface1badface2ULL),
+		.i_mtime	= spectime(0xbadface1badf0000ULL),
+		);
+	struct inode *inode2 = rapid_open_inode(sb, NULL, 0x666);
+
+	inode1->btree = (struct btree){
+		.root = { .block = 0xcaba1f00dULL, .depth = 3 },
+	};
+
+	char *p, attrs[1000] = { };
+	int size;
+
+	/* encode inode1 to attrs, then decode attrs to inode2 */
+	size = encode_asize(tux_inode(inode1)->present);
+	p = encode_attrs(inode1, attrs, size);
+	test_assert(p - attrs == size);
+	p = decode_attrs(inode2, attrs, size);
+	test_assert(p - attrs == size);
+
+	/* Compare inode1 and inode2 */
+	test_assert(inode1->present == inode2->present);
+	test_assert(inode1->i_rdev == inode2->i_rdev);
+	test_assert(inode1->i_mode == inode2->i_mode);
+	test_assert(inode1->i_uid == inode2->i_uid);
+	test_assert(inode1->i_gid == inode2->i_gid);
+	test_assert(inode1->i_size == inode2->i_size);
+	test_assert(inode1->i_ctime.tv_sec == inode2->i_ctime.tv_sec);
+	test_assert(inode1->i_ctime.tv_nsec == inode2->i_ctime.tv_nsec);
+	test_assert(inode1->i_mtime.tv_sec == inode2->i_mtime.tv_sec);
+	test_assert(inode1->i_mtime.tv_nsec == inode2->i_mtime.tv_nsec);
+	test_assert(inode1->btree.root.block == inode2->btree.root.block);
+	test_assert(inode1->btree.root.depth == inode2->btree.root.depth);
+
+	free_map(inode1->map);
+	free_map(inode2->map);
+}
+
 int main(int argc, char *argv[])
 {
-	unsigned abits = DATA_BTREE_BIT|CTIME_SIZE_BIT|MODE_OWNER_BIT|LINK_COUNT_BIT|MTIME_BIT;
 	struct dev *dev = &(struct dev){ .bits = 9 };
 
 	struct disksuper super = INIT_DISKSB(dev->bits, 100);
@@ -26,19 +76,11 @@ int main(int argc, char *argv[])
 	sb->super = super;
 	setup_sb(sb, &super);
 
-	struct inode *inode = rapid_open_inode(sb, NULL, 0x666,
-		.present = abits, .i_uid = 0x12121212, .i_gid = 0x34343434,
-		.i_size = 0x123456789ULL,
-		.i_ctime = spectime(0xdec0debeadULL),
-		.i_mtime = spectime(0xbadfaced00dULL));
-	inode->btree = (struct btree){
-		.root = { .block = 0xcaba1f00dULL, .depth = 3 }
-	};
+	test_init(argv[0]);
 
-	char attrs[1000] = { };
-	printf("need %i attr bytes\n", encode_asize(abits));
-	printf("decode %ti attr bytes\n", sizeof(attrs));
-	decode_attrs(inode, attrs, sizeof(attrs));
-	dump_attrs(inode);
-	exit(0);
+	if (test_start("test01"))
+		test01(sb);
+	test_end();
+
+	return test_failures();
 }
