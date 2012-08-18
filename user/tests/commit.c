18cfc2535e3339b90c6ad7417d58f78c5cf996b0
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2012-08-18 23:57:47 +0900

    Add test to truncate/unlink()
    This tests truncate/unlink(), and checks if freed block is not dirty anymore.
    
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>

--- user/tests/commit.c
+++ user/tests/commit.c
@@ -14,10 +14,50 @@
 
 #define trace trace_on
 
-/* cleanup of main() */
-static void clean_main(struct sb *sb)
+static block_t check_block;
+
+static int check_defree_block(struct sb *sb, u64 val)
 {
-	put_super(sb);
+	block_t block = val & ~(-1ULL << 48);
+	int count = val >> 48;
+	if (block <= check_block && check_block < block + count)
+		return -1;
+	return 0;
+}
+
+/* Check if buffer is not freed blocks */
+static int buffer_is_allocated(struct sb *sb, struct buffer_head *buf)
+{
+	check_block = bufindex(buf);
+	if (stash_walk(sb, &sb->defree, check_defree_block) < 0)
+		return -1; /* buffer is defree block */
+	if (stash_walk(sb, &sb->derollup, check_defree_block) < 0)
+		return -1; /* buffer is derollup block */
+
+	block_t block = balloc_from_range(sb, bufindex(buf), 1, 1);
+	if (block != -1) {
+		bfree(sb, block, 1);
+		return -1; /* buffer is freed block */
+	}
+	return 0;
+}
+
+static void check_dirty_list(struct sb *sb, struct list_head *head)
+{
+	struct buffer_head *buf, *n;
+	list_for_each_entry_safe(buf, n, head, link)
+		test_assert(buffer_is_allocated(sb, buf) == 0);
+}
+
+/*
+ * Check if freed blocks is not dirty
+ * FIXME: this should move into put_super()?
+ */
+static void check_dirty(struct sb *sb)
+{
+	/* volmap only, because data buffers doesn't have block address yet */
+	check_dirty_list(sb, &mapping(sb->volmap)->dirty);
+	check_dirty_list(sb, &sb->pinned);
 }
 
 struct open_result {
@@ -63,6 +103,15 @@ static void check_files(struct sb *sb, struct open_result *results, int nr)
 	}
 }
 
+/* cleanup of main() */
+static void clean_main(struct sb *sb)
+{
+	/* Check if it didn't make strange dirty buffer */
+	check_dirty(sb);
+
+	put_super(sb);
+}
+
 /* Generate all type of logs, and replay. */
 static void test01(struct sb *sb)
 {
@@ -96,6 +145,26 @@ static void test01(struct sb *sb)
 		test_assert(!IS_ERR(inode));
 		r->err = 0;
 		r->inum = inode->inum;
+
+		/*
+		 * This should make at least:
+		 * LOG_BNODE_DEL, LOG_BNODE_ADJUST
+		 *
+		 * FIXME: to generate LOG_BNODE_MERGE, this should use
+		 * punch hole, instead of truncate(). Then, read back
+		 * to check punch hole working.
+		 */
+		if (i == NUM_FILES - 1) {
+			struct file *file = &(struct file){ .f_inode = inode };
+			char data[1024] = {};
+			for (int j = 0; j < 1024; j++) {
+				int size = tuxwrite(file, data, sizeof(data));
+				test_assert(size == sizeof(data));
+				/* commit to generates many extents */
+				test_assert(force_delta(sb) == 0);
+			}
+			test_assert(tuxtruncate(inode, 0) == 0);
+		}
 		iput(inode);
 
 		if ((i % 10) == 0)
@@ -107,6 +176,8 @@ static void test01(struct sb *sb)
 		snprintf(r->name, sizeof(r->name), "file%03d", i);
 		r->err = -ENOENT;
 	}
+
+	check_dirty(sb);
 
 	if (test_start("test01.1")) {
 		test_assert(force_delta(sb) == 0);
@@ -133,6 +204,113 @@ static void test01(struct sb *sb)
 	test_end();
 
 	test_assert(force_delta(sb) == 0);
+	clean_main(sb);
+}
+
+/* Test to unlink file before flushing */
+/*  FIXME: check if I/O is nothing for inode */
+static void test02(struct sb *sb)
+{
+	static struct open_result r;
+
+	struct tux_iattr iattr = { .mode = S_IFREG | S_IRWXU };
+
+	test_assert(make_tux3(sb) == 0);
+	test_assert(force_rollup(sb) == 0);
+
+	r.namelen = snprintf(r.name, sizeof(r.name), "file%03d", 1);
+	r.err = -ENOENT;
+
+	/* Create inode and write data without flush */
+	struct inode *inode;
+	inode = tuxcreate(sb->rootdir, r.name, r.namelen, &iattr);
+	test_assert(!IS_ERR(inode));
+
+	struct file *file = &(struct file){ .f_inode = inode };
+	char data[1024] = {};
+	for (int i = 0; i < 1024; i++) {
+		int size = tuxwrite(file, data, sizeof(data));
+		test_assert(size == sizeof(data));
+	}
+	iput(inode);
+	/* unlink created inode */
+	test_assert(tuxunlink(sb->rootdir, r.name, r.namelen) == 0);
+	check_dirty(sb);
+
+	/* Flush */
+	test_assert(force_delta(sb) == 0);
+	clean_main(sb);
+
+	/* FIXME: fsck please */
+	/* fsck(); */
+
+	check_files(sb, &r, 1);
+	clean_main(sb);
+}
+
+/* Test to unlink file after flushing */
+static void test03(struct sb *sb)
+{
+	static struct open_result r;
+
+	struct tux_iattr iattr = { .mode = S_IFREG | S_IRWXU };
+
+	test_assert(make_tux3(sb) == 0);
+	test_assert(force_rollup(sb) == 0);
+
+	r.namelen = snprintf(r.name, sizeof(r.name), "file%03d", 1);
+	r.err = -ENOENT;
+
+	/* Create inode and write data without flush */
+	struct inode *inode;
+	inode = tuxcreate(sb->rootdir, r.name, r.namelen, &iattr);
+	test_assert(!IS_ERR(inode));
+
+	struct file *file = &(struct file){ .f_inode = inode };
+	char data[1024] = {};
+	for (int i = 0; i < 1024; i++) {
+		int size = tuxwrite(file, data, sizeof(data));
+		test_assert(size == sizeof(data));
+	}
+	iput(inode);
+	test_assert(force_delta(sb) == 0);
+
+	if (test_start("test03.1")) {
+		/* unlink created inode */
+		test_assert(tuxunlink(sb->rootdir, r.name, r.namelen) == 0);
+		check_dirty(sb);
+
+		/* Flush */
+		test_assert(force_delta(sb) == 0);
+		clean_main(sb);
+
+		/* FIXME: fsck please, check leak */
+		/* fsck(); */
+
+		check_files(sb, &r, 1);
+		clean_main(sb);
+	}
+	test_end();
+
+	if (test_start("test03.2")) {
+		test_assert(force_rollup(sb) == 0);
+
+		/* unlink created inode */
+		test_assert(tuxunlink(sb->rootdir, r.name, r.namelen) == 0);
+		check_dirty(sb);
+
+		/* Flush */
+		test_assert(force_rollup(sb) == 0);
+		clean_main(sb);
+
+		/* FIXME: fsck please, check leak */
+		/* fsck(); */
+
+		check_files(sb, &r, 1);
+		clean_main(sb);
+	}
+	test_end();
+
 	clean_main(sb);
 }
 
@@ -164,6 +342,14 @@ int main(int argc, char *argv[])
 
 	if (test_start("test01"))
 		test01(sb);
+	test_end();
+
+	if (test_start("test02"))
+		test02(sb);
+	test_end();
+
+	if (test_start("test03"))
+		test03(sb);
 	test_end();
 
 	clean_main(sb);
